bf9804a5c31fb0cebc648a6d876fe137f809062b
Ralph Fuentes <academy4@talentamp.com>
2024-03-22 15:55:20 +0800

    Utilized MUI for css. Will still work on this

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,5 +1,14 @@
-import React, { useState } from 'react';
-import { Button, TextField, Typography } from '@mui/material';
+import React, { useState } from "react";
+import {
+  Button,
+  Grid,
+  TextField,
+  Typography,
+  Box,
+  Paper,
+  List,
+  ListItemText,
+} from "@mui/material";
 
 interface Todo {
   id: number;
@@ -22,7 +31,7 @@ export const TodoList = () => {
   };
 
   const handleDeleteTodo = (id: number) => {
-    setTodos(todos.filter(todo => todo.id !== id));
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   const handleEditTodo = (id: number, newText: string) => {
@@ -37,32 +46,75 @@ export const TodoList = () => {
   };
 
   const handleToggleEdit = (id: number) => {
-    const newText = prompt("Edit todo:", todos.find(todo => todo.id === id)?.text || "") || "";
+    const newText =
+      prompt("Edit todo:", todos.find((todo) => todo.id === id)?.text || "") ||
+      "";
     if (newText.trim() !== "") {
       handleEditTodo(id, newText);
     }
   };
 
   return (
-    <div className='main-container'>
-      <Typography variant="h4">TodoList</Typography>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <span>{todo.text}</span>
-            <Button variant="contained" onClick={() => handleToggleEdit(todo.id)}>Edit</Button>
-            <Button variant="contained" onClick={() => handleDeleteTodo(todo.id)}>Delete</Button>
-          </li>
-        ))}
-      </ul>
-      <TextField
-        type="text"
-        label='Add todo item'
-        value={input}
-        onChange={(e) => setInput(e.currentTarget.value)}
-      />
-      <Button variant="contained" onClick={handleAddTodo}>Add</Button>
-    </div>
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      sx={{ height: "100vh", bgcolor: "#808080" }}
+    >
+      <Grid item xs={12} sm={8} md={6}>
+        <Paper
+          sx={{
+            paddingInline: "20px",
+            paddingBlock: "30px",
+            borderRadius: "8px",
+          }}
+          elevation={3}
+        >
+          <Typography variant="h4" textAlign="center">
+            TodoList
+          </Typography>
+
+          <Box sx={{ paddingInline: "50px", paddingBlock: "20px" }}>
+            <List sx={{ display: "grid", gap: "10px" }}>
+              {todos.map((todo) => (
+                <ListItemText key={todo.id}>
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Typography variant="h6">{todo.text}</Typography>
+                    <Box sx={{ display: "flex", gap: "10px" }}>
+                      <Button
+                        variant="contained"
+                        onClick={() => handleToggleEdit(todo.id)}
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        variant="contained"
+                        onClick={() => handleDeleteTodo(todo.id)}
+                      >
+                        Delete
+                      </Button>
+                    </Box>
+                  </Box>
+                </ListItemText>
+              ))}
+            </List>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <TextField
+                type="text"
+                label="Add todo item"
+                value={input}
+                onChange={(e) => setInput(e.currentTarget.value)}
+              />
+              <Button variant="contained" onClick={handleAddTodo}>
+                Add
+              </Button>
+            </Box>
+          </Box>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
 
