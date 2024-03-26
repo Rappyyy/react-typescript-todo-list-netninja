8962bcb9bf78ed94d49423238d15b8993f915bb6
Ralph Fuentes <academy4@talentamp.com>
2024-03-26 17:01:40 +0800

    Currently working with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import {TodoList} from "./TodoList"
+import { TodoList } from "./TodoList";
 
 function App() {
-  
-
   return (
-   <div className="App"><TodoList/></div>
-    
-  )
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+export default App;
